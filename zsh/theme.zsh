412591ee50bc092a1ac5d13ba2bd525cf7b7faa3
Simon Albrecht <simon@albrecht.me>
2019-03-10 01:12:59 +0100

    Configure spaceship prompt

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,6 +1,7 @@
 # https://github.com/denysdovhan/spaceship-prompt
 antibody bundle https://github.com/denysdovhan/spaceship-prompt
 
+SPACESHIP_HOST_SHOW_FULL="true"
 SPACESHIP_HOST_PREFIX="@"
 SPACESHIP_DIR_PREFIX=''
 
